cad8dcd12db46f8c81e951285ecd175a545c3224
ismaellc <ismaelc_10@hotmail.com>
2016-03-02 11:21:39 -0600

    Update composicion.formas.js

--- rv/composicion.formas.js
+++ rv/composicion.formas.js
@@ -1,10 +1,10 @@
 function setup() {
 //se plantean las geometrias a utilizar
-var esferaForma=new THREE.SphereGeometry(1);
-var cilindroForma=new THREE.CylinderGeometry(0.5,0.5,4);
+var esferaForma = new THREE.SphereGeometry(1);
+var cilindroForma = new THREE.CylinderGeometry(0.5,0.5,4);
 //para generar una forma combinada se requiere de las mallas para poder desplazar las formas en el espacio virtual.
-var esfera1=new THREE.Mesh(esferaForma);
-var esfera2= new THREE.Mesh(esferaForma);
+var esfera1 = new THREE.Mesh(esferaForma);
+var esfera2 = new THREE.Mesh(esferaForma);
 var cilindro = new THREE.Mesh(cilindroForma);
 // se desplazan las mallas.
 esfera1.position.y=2;
@@ -16,14 +16,14 @@ THREE.GeometryUtils.merge(forma,esfera1);
 THREE.GeometryUtils.merge(forma,esfera2);
 THREE.GeometryUtils.merge(forma,cilindro);
 //se genera la malla a partir de la forma.
-malla =new THREE.Mesh(forma);
-wireframe=new THREE.WireframeHelper(malla, 0x00ff00);
+malla = new THREE.Mesh(forma);
+wireframe = new THREE.WireframeHelper(malla, 0x00ff00);
 var escena= new THREE.Scene();
 escena.add(malla,wireframe);
 //se inicializa la camara y el *renderer*.
-camara =new THREE.PerspectiveCamera();
+camara = new THREE.PerspectiveCamera();
 camara.position.z=10;
-var renderer=new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderer.domElement);
 }
