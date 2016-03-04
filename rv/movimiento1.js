57616a33d562c8b6cbf36034e4cd6d64abf63add
ismaellc <ismaelc_10@hotmail.com>
2016-03-04 14:23:15 -0600

    Update movimiento1.js

--- rv/movimiento1.js
+++ rv/movimiento1.js
@@ -3,8 +3,8 @@ THREE.Object3D.call(this);
 this.pierna =new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
 this.pie =new THREE.Mesh(new THREE.BoxGeometry(2,1,1));
 this.pierna.position.y=-2.5;
-this.pie.potition.y=-4.5;
-this.pie.potition.x=1;
+this.pie.position.y=-4.5;
+this.pie.position.x=1;
 this.add(this.pierna);
 this.add(this.pie);
 }
