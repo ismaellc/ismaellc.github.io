16abb0ca3e918dd4f8e7b8af184f6be2cede68fe
ismaellc <ismaelc_10@hotmail.com>
2016-03-16 10:33:52 -0600

    Create texturas2.js

---
+++ rv/2016_03_16/texturas2.js
@@ -0,0 +1,29 @@
+function setup(){
+ THREE.ImageUtils.crossOrigin='';
+ var textura=THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/brick_diffuse.jpg');
+ var material=new THREE.MeshLambertMaterial({map:textura});
+ var forma=new THREE.BoxGeometry(1,4,9);
+ malla=new THREE.Mesh(forma,material);
+
+escena= new THREE.Scene();
+escena.add(malla);
+
+camara=new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+renderer=new THREE.WebGLRenderer();
+renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderer.domElement);
+}
+
+function loop(){
+  requestAnimationFrame(loop);
+  malla.rotation.x += 0.01;
+  malla.rotation.y += 0.01;
+  renderer.render(escena,camara);
+}
+
+var camara,escena,renderer,malla;
+
+setup();
+loop();
