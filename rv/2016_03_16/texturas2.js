1aea6c91fa74d1d80f3ebc6e4e7860958e8afbbc
ismaellc <ismaelc_10@hotmail.com>
2016-03-16 10:39:39 -0600

    Update texturas2.js

--- rv/2016_03_16/texturas2.js
+++ rv/2016_03_16/texturas2.js
@@ -15,7 +15,7 @@ var luzPuntual=new THREE.PointLight(0xffffff);
 	escena.add(luzPuntual);
 
 camara=new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=15;
 
 renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
