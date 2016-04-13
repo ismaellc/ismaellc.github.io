9c409c56e5f80cf9ebfee3806b19241c778af9a9
ismaellc <ismaelc_10@hotmail.com>
2016-04-13 10:42:37 -0500

    Update ejemplo.2.js

--- rv/2016_04_13/ejemplo.2.js
+++ rv/2016_04_13/ejemplo.2.js
@@ -1,1 +1,38 @@
+function Wall(size,x,y){
+  THREE.Mesh.call(this,new THREE.BoxGeometry(size,size,size),new THREE.MeshNormalMaterial());
+  this.size=size;
+  this.position.x=x;
+  this.position.y=y;
+}
+Wall.prototype=new THREE.Mesh();
 
+Environment.prototype.setMap=fuction(map){
+  var _offset=Math.floor(map.length/2);
+  for(var i=0;i<map.length;i++)
+  for(var j=0;j<map.length;j++){
+    if(map[i][j]==="x")
+    this.add(new Wall(1,j-_offset,-(i-_offset)));
+    else if(map[i][j]==="r")
+    this.add(new Robot(0.5,j-_offset,-(i-_offset)));
+  }
+}
+
+function setup(){
+  var map = new Array();
+  mapa[0] = "xxxxxxxxxxxxxxxxxxxx";
+  mapa[1] = "xr                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+  mapa[] = "x                 x";
+}
