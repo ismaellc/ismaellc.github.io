42ba1e3cc3b8ac545a63bf7f8ac310cf3500ea10
ismaellc <ismaelc_10@hotmail.com>
2016-04-13 22:38:33 -0500

    Update robots.js

--- rv/2016_04_13/robots.js
+++ rv/2016_04_13/robots.js
@@ -1,17 +1,22 @@
+function Sensor(position, direction) {
+  THREE.Raycaster.call(this, position, direction);
+  this.colision = false;
+}
+
+Sensor.prototype = new THREE.Raycaster();
+
 function Robot (size, x,y){
   Agent.call(this,x,y);
-  
-  this.sensor= new Sensor();
-  this.actuator= new THREE.Mesh(new THREE.BoxGeometry(size,size,size),new THREE.MeshBasicMaterial({color:'#aa0000'}));
+  this.sensor = new Sensor();
+  this.actuator = new THREE.Mesh(new THREE.BoxGeometry(size,size,size),new THREE.MeshBasicMaterial({color:'#aa0000'}));
   this.actuator.commands=[];
   this.add(this.actuator);
 }
 
 Robot.prototype=new Agent();
 
-Robot.prototype.sense= function(environment){
-  this.sensor.set(this.position,
-  new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),0));
+Robot.prototype.sense = function(environment){
+  this.sensor.set(this.position, new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),0));
   var obstaculo= this.sensor.intersectObjects(environment.children.true);
   if((obstaculo.length>0 && (obstaculo[0].distance<=.5)))
     this.sensor.colision=true;
@@ -29,10 +34,11 @@ Robot.prototype.act=function(environment){
   var command=this.actuator.commands.pop();
   if( command==undefined)
     console.log('Undefined command');
-  else if(commands in this.operations)
+  else if(command in this.operations)
     this.operations[command](this);
   else
     console.log('Unknown command');
+}
   
   //las operaciones posibles con este robot son
   //goStraight()
@@ -40,12 +46,13 @@ Robot.prototype.act=function(environment){
   //rotateCCW()
   
   Robot.prototype.operations={};
+  
   Robot.prototype.operations.goStraight= function(robot, distance){
   if(distance== undefined)
     distance= .05;
   robot.position .x+= distance*Math.cos(robot.rotation.z);
   robot.position .y+= distance*Math.sin(robot.rotation.z);
-  }
+  
 }
 
 Robot.prototype.operations.rotateCW= function (robot,angle){
